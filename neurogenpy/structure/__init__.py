f6b835b8109171a9d324ae107fb3fee33b61347a
javiegal <j.gallego.gut@gmail.com>
2022-07-05 19:57:28 +0200

    Minor changes.

--- neurogenpy/structure/__init__.py
+++ neurogenpy/structure/__init__.py
@@ -4,65 +4,76 @@
 # Licensed under GNU General Public License v3.0:
 # https://www.gnu.org/licenses/gpl-3.0.html
 
-from rpy2.robjects.packages import importr, isinstalled
-from rpy2.robjects.vectors import StrVector
+import logging
+from subprocess import check_call, CalledProcessError
 
-from .cl import CL
-from .fast_iamb import FastIamb
 from .fges import FGES
 from .fges_merge import FGESMerge
 from .genie3 import GENIE3
 from .graphical_lasso import GraphicalLasso
-from .grow_shrink import GrowShrink
-from .hc_tabu import HcTabu
-from .hill_climbing import HillClimbing
-from .hiton_pc import HitonPC
-from .iamb import Iamb
-from .inter_iamb import InterIamb
-from .learn_structure import LearnStructure
 from .lr import Lr
 from .mbc import MBC
 from .mi_continuous import MiContinuous
-from .mmhc import MMHC
-from .mmpc import MMPC
-from .naive_bayes import NB
-from .pc import PC
 from .pearson import Pearson
-from .sparsebn import SparseBn
-from .tan import Tan
+
+logger = logging.getLogger(__name__)
 
 __all__ = [
-    'CL',
-    'FastIamb',
     'FGES',
     'FGESMerge',
     'GENIE3',
     'GraphicalLasso',
-    'GrowShrink',
-    'HillClimbing',
-    'HcTabu',
-    'HitonPC',
-    'Iamb',
-    'InterIamb',
     'Lr',
     'MBC',
     'MiContinuous',
-    'MMHC',
-    'MMPC',
-    'NB',
-    'PC',
     'Pearson',
-    'SparseBn',
-    'Tan',
-    'LearnStructure'
 ]
 
-# Install R packages sparsebn and bnlearn
-# utils = importr('utils')
-# utils.chooseCRANmirror(ind=1)
-#
-# packnames = ('sparsebn', 'bnlearn')
-# names_to_install = [x for x in packnames if not isinstalled(x)]
-#
-# if names_to_install:
-#     utils.install_packages(StrVector(names_to_install))
+try:
+    # TODO: Check R installation in another way. It does not work for Windows.
+    check_call(['which', 'R'])
+except CalledProcessError:
+    logger.warning('R installation is needed. Multiple structure learning '
+                   'methods methods use it.')
+else:
+    from rpy2.robjects.packages import isinstalled
+
+    from .cl import CL
+    from .fast_iamb import FastIamb
+    from .grow_shrink import GrowShrink
+    from .hc_tabu import HcTabu
+    from .hill_climbing import HillClimbing
+    from .hiton_pc import HitonPC
+    from .iamb import Iamb
+    from .inter_iamb import InterIamb
+    from .mmhc import MMHC
+    from .mmpc import MMPC
+    from .naive_bayes import NB
+    from .pc import PC
+    from .sparsebn import SparseBn
+    from .tan import Tan
+
+    __all__ += [
+        'CL',
+        'FastIamb',
+        'GrowShrink',
+        'HillClimbing',
+        'HcTabu',
+        'HitonPC',
+        'Iamb',
+        'InterIamb',
+        'MMHC',
+        'MMPC',
+        'NB',
+        'PC',
+        'SparseBn',
+        'Tan',
+    ]
+
+    packnames = ('sparsebn', 'bnlearn')
+
+    for package in packnames:
+        if not isinstalled(package):
+            logger.warning(
+                f'R library {package} is not installed and some structure '
+                f'learning methods rely on it. Please, install it!')
